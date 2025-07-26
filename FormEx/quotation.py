725b974c9d78402810aff95394703fe8562af010
Bot.Seven <61865878+boattepin15@users.noreply.github.com>
2025-07-26 10:39:16 +0700

    Add files via upload

--- FormEx/quotation.py
+++ FormEx/quotation.py
@@ -1,85 +1,69 @@
 from core.mixins.viewMixins import (
     BaseListView,
-    BaseCreateView          
+    BaseCreateView,
+    BaseUpdateView,
+    BaseDeleteView          
 )
-from ..models import Quotation
+from quotation.models import Quotation, QuotationItem
 from django.views.generic import DetailView
 from ..forms.quotation import QuotationForm, QuotationItemInlineFormSet
 from django.contrib.auth.mixins import LoginRequiredMixin
 from core.mixins.formset import DynamicFormSetView
 from django.urls import reverse_lazy
+from itertools import groupby
+from operator import attrgetter
+
 
 class QuotationListView(BaseListView):
     model = Quotation
     template_name = 'quotation/quotation_list.html'
     context_object_name = 'quotations'
-class QuotationPrintView(LoginRequiredMixin, DetailView):
-    """
-    แสดงใบเสนอราคาในรูปแบบ “พร้อมพิมพ์”  (Printable view)
 
-    ✓  Prefetch `items` เพื่อลด query
-    ✓  ส่งยอดรวม (subtotal / discount / vat / grand_total) พร้อมไปใน context
-    ✓  แนบข้อมูลบริษัท (Pacharasub) ไว้ใน context ให้ template ใช้งานได้ครบทุกช่อง
-    """
-
-    model = Quotation
-    template_name = "quotation/print_quotation.html"   
-    context_object_name = "quotation"
-
-    # --------------------------------------------------------------------- #
-    # optimise queries                                                      #
-    # --------------------------------------------------------------------- #
-    def get_queryset(self):
-        return (
-            Quotation.objects
-            .select_related("created_by")        # ถ้าต้องโชว์ผู้สร้าง
-            .prefetch_related("items")           # รายการสินค้า / service
-        )
-
-    # --------------------------------------------------------------------- #
-    # context                                                               #
-    # --------------------------------------------------------------------- #
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        q = self.object
-
-        # ข้อมูลรายการ
-        ctx["items"] = q.items.all()
-
-        # ยอดรวมต่าง ๆ
-        ctx["subtotal"]     = q.subtotal
-        ctx["discount"]     = q.discount
-        ctx["vat"]          = q.vat
-        ctx["grand_total"]  = q.grand_total
-        return ctx
-    
-class QuotationCreateView(BaseCreateView):
-    """
-    สร้างใบเสนอราคา + รายการสินค้า/บริการ (inline formset)
-    """
+class BaseQuotationView:
     model = Quotation
     form_class = QuotationForm
     template_name = "quotation/quotation_form.html"
     success_url = reverse_lazy("quotation:quotation_list")
-
-    # ---------- inline formset ----------
     formset_class = QuotationItemInlineFormSet
     formset_names = ("item_set",)
 
-    # ---------- extra context ----------
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # เตรียม formset สำหรับตารางรายการ
-        if self.request.method == "POST":
-            context["item_set"] = self.formset_class(
-                self.request.POST, self.request.FILES, prefix="item_set"
-            )
-        else:
-            context["item_set"] = self.formset_class(prefix="item_set")
-
-        context["page_title"] = "สร้างใบเสนอราคา"
+        context["item_set"] = self.get_item_formset()
         return context
+    
+
+class QuotationCreateView(BaseQuotationView, BaseCreateView):
+    pass
+
+class QuotationUpdateView(BaseQuotationView, BaseUpdateView):
+    pass
+
+class QuotationDeleteView(BaseDeleteView):
+    model = Quotation
+    success_url = reverse_lazy("quotation:quotation_list")
+    success_message = "ลบใบเสนอราคาแล้ว"
+
+class QuotationPrintView(LoginRequiredMixin, DetailView):
+    model = Quotation
+    template_name = "quotation/print_quotation.html"
+    context_object_name = "quotation"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        quotation = self.object
+
+        # โหลดรายการทั้งหมดที่เกี่ยวข้องกับใบเสนอราคานี้
+        items = QuotationItem.objects.filter(quotation=quotation).order_by("section_title", "id")
+
+        # Group by section_title (groupby ต้องเรียงมาก่อน)
+        grouped_items = []
+        for section, group in groupby(items, key=attrgetter("section_title")):
+            grouped_items.append((section, list(group)))
+
+        context["grouped_items"] = grouped_items
+        return context
+    
 
 class QuotationItemFormsetView(DynamicFormSetView):
     """
